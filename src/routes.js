9677426f2b55d6152d07b9e663405353cb69f77a
sai vanka <vjolamuthyasai@gmail.com>
2023-04-01 11:48:06 +0530

    feat: updated projects path

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import AboutContainer from './containers/About/AboutContainer';
 import HomeContainer from './containers/Home/HomeContainer';
+import ProjectContainer from './containers/Project/ProjectsContainer';
 import ResumeContainer from './containers/Resume/ResumeContainer';
 import RootLayout from './layouts/RootLayout';
-
 export const Router = () => {
   const routes = useRoutes([
     {
       path: '/',
       element: <RootLayout />,
       children: [
-        { path: '/', element: <HomeContainer /> },
+        { index: true, element: <HomeContainer /> },
         { path: '/about', element: <AboutContainer /> },
         { path: '/resume', element: <ResumeContainer /> },
+        { path: '/project', element: <ProjectContainer /> },
       ],
     },
   ]);
